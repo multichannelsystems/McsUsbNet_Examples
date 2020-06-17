582def829e2f48b3725dd418b6db5194aa962903
Peter Jesinger <jesinger@multichannelsystems.com>
2020-06-17 10:49:00 +0200

    updated pyton Stimulation.py example

--- Examples/Python/Stimulation.py
+++ Examples/Python/Stimulation.py
@@ -8,13 +8,12 @@ clr.AddReference('c:\\Programming\\DllNet\\x64\\Release\\McsUsbNet.dll')
 from Mcs.Usb import CMcsUsbListNet
 from Mcs.Usb import DeviceEnumNet
 
-from Mcs.Usb import CMeaDeviceNet
+from Mcs.Usb import CStg200xDownloadNet
 from Mcs.Usb import McsBusTypeEnumNet
-from Mcs.Usb import DataModeEnumNet
-from Mcs.Usb import SampleSizeNet
+from Mcs.Usb import STG_DestinationEnumNet
 
-def PollHandler(status, stgStatusNet, index_list)
-    print status, stgStatusNet, index_list
+def PollHandler(status, stgStatusNet, index_list):
+    print('%x %s' % (status, str(stgStatusNet.TiggerStatus[0])))
 
 deviceList = CMcsUsbListNet(DeviceEnumNet.MCS_DEVICE_USB)
 
@@ -31,23 +30,25 @@ device.Stg200xPollStatusEvent += PollHandler;
 
 device.Connect(deviceList.GetUsbListEntry(0))
 
-device.SetVoltageMode();
 voltageRange = device.GetVoltageRangeInMicroVolt(0);
 voltageResulution = device.GetVoltageResolutionInMicroVolt(0);
+currentRange = device.GetCurrentRangeInNanoAmp(0);
+currentResolution = device.GetCurrentResolutionInNanoAmp(0);
 
-print(voltageRange, voltageResulution)
+print('Voltage Mode:  Range: %d mV  Resolution: %1.2f mV' % (voltageRange/1000, voltageResulution/1000.0))
+print('Current Mode:  Range: %d uA  Resolution: %1.2f uA' % (currentRange/1000, currentResolution/1000.0))
 
 channelmap = Array[UInt32]([1,0,0,0])
 syncoutmap = Array[UInt32]([1,0,0,0])
 repeat = Array[UInt32]([10,0,0,0])
 
-amplitude = Array[Int32](-100,100);
-duration = Array[UInt64](100,100);
+amplitude = Array[Int32]([-100,100]);
+duration = Array[UInt64]([10000,10000]);
 
 device.SetupTrigger(0, channelmap, syncoutmap, repeat)
-device.ClearChannelData(0)
-device.SendChannelData(0, amplitude, duration)
-device.SendStart(1) // Trigger 1 
-time.sleep(2)
+device.SetVoltageMode();
+device.PrepareAndSendData(0, amplitude, duration, STG_DestinationEnumNet.channeldata_voltage)
+device.SendStart(1) 
+time.sleep(10)
 
 device.Disconnect()

dc08ecfecaa9ec91f8f20ae23ed5a688a1ae7e1e
Jens Paetzold <paetzold@multichannelsystems.com>
2021-10-05 14:30:42 +0200

    Added C# Project for fast feedback via PC

---
+++ Examples/CSharp/Feedback/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Feedback
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.mcsdevices = new System.Windows.Forms.Label();
+            this.cbDeviceList = new System.Windows.Forms.ComboBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.btStop = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mcsdevices
+            // 
+            this.mcsdevices.AutoSize = true;
+            this.mcsdevices.Location = new System.Drawing.Point(12, 15);
+            this.mcsdevices.Name = "mcsdevices";
+            this.mcsdevices.Size = new System.Drawing.Size(75, 13);
+            this.mcsdevices.TabIndex = 1;
+            this.mcsdevices.Text = "MCS Devices:";
+            // 
+            // cbDeviceList
+            // 
+            this.cbDeviceList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDeviceList.FormattingEnabled = true;
+            this.cbDeviceList.Location = new System.Drawing.Point(93, 12);
+            this.cbDeviceList.Name = "cbDeviceList";
+            this.cbDeviceList.Size = new System.Drawing.Size(190, 21);
+            this.cbDeviceList.TabIndex = 2;
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(12, 57);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(75, 23);
+            this.btStart.TabIndex = 3;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // btStop
+            // 
+            this.btStop.Location = new System.Drawing.Point(93, 57);
+            this.btStop.Name = "btStop";
+            this.btStop.Size = new System.Drawing.Size(75, 23);
+            this.btStop.TabIndex = 4;
+            this.btStop.Text = "Stop";
+            this.btStop.UseVisualStyleBackColor = true;
+            this.btStop.Click += new System.EventHandler(this.btStop_Click);
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(12, 86);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(589, 352);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(607, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(181, 173);
+            this.listBox1.TabIndex = 6;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(607, 265);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(181, 173);
+            this.listBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.btStop);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.cbDeviceList);
+            this.Controls.Add(this.mcsdevices);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mcsdevices;
+        private System.Windows.Forms.ComboBox cbDeviceList;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.Button btStop;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Examples/CSharp/Feedback/Form1.cs
@@ -0,0 +1,246 @@
+﻿using Mcs.Usb;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Timer = System.Timers.Timer;
+
+
+namespace Feedback
+{
+    public partial class Form1 : Form
+    {
+        const int Channels = 32;
+        const int AnalogChannels = 8;
+        const int Checksum = 2;
+
+        private int TotalChannels = 0;
+
+        int Samplerate = 50000;
+        private int Threshold = 10;
+
+        Timer timer = new Timer();
+
+        CMcsUsbListNet UsbDeviceList = new CMcsUsbListNet(DeviceEnumNet.MCS_DEVICE_USB);
+        CMeaUSBDeviceNet mea = new CMeaUSBDeviceNet();
+
+        private int dig_count = 0;
+        private bool dig_data = true;
+
+        private int digitallength = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer.Elapsed += Timer_Elapsed;
+
+            mea.ErrorEvent += Mea_ErrorEvent;
+            mea.ChannelDataEvent += Mea_ChannelDataEvent;
+
+            UsbDeviceList.DeviceArrival += UsbDeviceList_DeviceArrivalRemoval;
+            UsbDeviceList.DeviceRemoval += UsbDeviceList_DeviceArrivalRemoval;
+
+            btStop.Enabled = false;
+
+            UpdateDeviceList();
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            CMeaDigitalDataFunctionNet dig = new CMeaDigitalDataFunctionNet(mea);
+
+            if (dig_count > 0)
+            {
+                dig_count--;
+                if (dig_count == 0)
+                {
+                    dig.SetDigitalData(0, true);
+                }
+            }
+        }
+
+        private void UsbDeviceList_DeviceArrivalRemoval(CMcsUsbListEntryNet entry)
+        {
+            UpdateDeviceList();
+        }
+
+        private void UpdateDeviceList()
+        {
+            cbDeviceList.Items.Clear();
+            cbDeviceList.Items.AddRange(UsbDeviceList.GetUsbListEntries());
+            if (cbDeviceList.Items.Count > 0)
+            {
+                cbDeviceList.SelectedIndex = 0;
+            }
+        }
+
+        private void Mea_ErrorEvent(string msg, int action)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new OnError(Mea_ErrorEvent), msg, action);
+            }
+            else
+            {
+                listBox2.Items.Add(msg);
+            }
+        }
+
+        private void Mea_ChannelDataEvent(CMcsUsbDacqNet dacq, int CbHandle, int numFrames)
+        {
+            CMeaDigitalDataFunctionNet dig = new CMeaDigitalDataFunctionNet(dacq);
+
+            int[] data = mea.ChannelBlock_ReadFramesI32(0, Threshold, out int frames_read);
+            for (int i = Channels + AnalogChannels; i < data.Length; i += TotalChannels)
+            {
+                int z = data[i];
+                if (z == 1)
+                {
+                    if (dig_data)
+                    {
+                        dig.SetDigitalData(0, false);
+                        dig_count = 10;
+                        dig_data = false;
+                        digitallength = 0;
+                    }
+
+                    digitallength++;
+                }
+                else
+                {
+                    if (!dig_data)
+                    {
+                        dig_data = true;
+                        BeginInvoke(new InfoDataAction(InfoData), digitallength);
+                    }
+                }
+            }
+        }
+
+        delegate void InfoDataAction(int length);
+
+        private double infolength = 0;
+        private double ninfolength = 0;
+        private double infolengthmax = 1;
+        private double infolengthmin = 10000;
+
+        private const int HISTO_LENGTH = 100;
+        private double[] infoHistogram = new double[HISTO_LENGTH];
+
+        void ClearInfo()
+        {
+            infolength = 0;
+            ninfolength = 0;
+            infolengthmax = 1;
+            infolengthmin = 10000;
+
+            infoHistogram = new double[HISTO_LENGTH];
+        }
+
+        void InfoData(int length)
+        {
+            double index = length / ((double)Samplerate / 1000.0);
+            infolength += index;
+            ninfolength += 1;
+            if (index > infolengthmax) infolengthmax = index;
+            if (index < infolengthmin) infolengthmin = index;
+            listBox1.Items.Clear();
+            listBox1.Items.Add(index.ToString());
+            listBox1.Items.Add((infolength / ninfolength).ToString());
+            listBox1.Items.Add(infolengthmax.ToString());
+            listBox1.Items.Add(infolengthmin.ToString());
+
+            int index_i = (int)Math.Round(index);
+            infoHistogram[index_i < HISTO_LENGTH ? index_i : HISTO_LENGTH - 1] += 1;
+
+            chart1.Series[0].Points.Clear();
+            int max;
+            for (max = HISTO_LENGTH - 1; max >= 0; max--)
+            {
+                if (infoHistogram[max] > 0)
+                {
+                    break;
+                }
+            }
+
+            max = (max * 10 + 10)/ 10;
+            for (int i = 0; i < max; i++)
+            {
+                chart1.Series[0].Points.Add(infoHistogram[i]);
+            }
+        }
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            if (mea.Connect((CMcsUsbListEntryNet) cbDeviceList.SelectedItem) == 0)
+            {
+                mea.SetDataMode(DataModeEnumNet.Signed_32bit, 0);
+                if (mea.GetDeviceId().IdProduct == ProductIdEnumNet.W2100)
+                {
+                    Samplerate = 20000;
+                }
+
+                mea.SetNumberOfAnalogChannels(Channels, 0, 0, AnalogChannels, 0); // Read raw data
+
+                try
+                {
+                    mea.SetSamplerate(Samplerate, 1, 0);
+                }
+                catch (CUsbExceptionNet)
+                {
+                    Samplerate = mea.GetSamplerate(0);
+                }
+
+                mea.EnableDigitalIn(DigitalDatastreamEnableEnumNet.RegisterLow | DigitalDatastreamEnableEnumNet.RegisterLow, 0);
+
+                mea.SetDigitalSource(DigitalTargetEnumNet.Digout, 0, SCUDigitalSourceEnumNet.DigitalData, 0);
+
+                mea.EnableChecksum(true, 0);
+                int ChannelsInBlock = mea.GetChannelsInBlock(0);
+
+                mea.GetChannelLayout(out int analogChannels, out int digitalChannels, out int checksumChannels, out int timestampChannels, out int channelsInBlock, 0);
+
+                TotalChannels = channelsInBlock / 2;
+                mea.SetSelectedData(TotalChannels, Samplerate * 10, Threshold, SampleSizeNet.SampleSize32Signed, ChannelsInBlock);
+
+                mea.ChannelBlock_SetCheckChecksum((uint)checksumChannels, (uint)timestampChannels);
+
+                CMeaDigitalDataFunctionNet dig = new CMeaDigitalDataFunctionNet(mea);
+                dig.SetDigitalData(0, false);
+                dig_count = 10;
+                dig_data = true;
+
+                timer.Interval = 9;
+                timer.Enabled = true;
+
+                ClearInfo();
+
+                //mea.StartDacq(-1, 32, 64, 128, 0);
+                mea.StartDacq(-1, 128, 256, 16, 0);
+
+                btStop.Enabled = true;
+                btStart.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("Could not connect");
+            }
+        }
+
+        private void btStop_Click(object sender, EventArgs e)
+        {
+
+            mea.StopDacq();
+
+            mea.Disconnect();
+            btStart.Enabled = true;
+            btStop.Enabled = false;
+
+        }
+    }
+}

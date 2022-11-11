ac4e24f29be40219f4bf351764ce97dfcd90d678
Jens Paetzold <paetzold@multichannelsystems.com>
2022-11-11 09:43:26 +0100

    dacq is running in new example

--- Examples/CSharp/MEA2100_Recording_and_Stimulation/Form1.cs
+++ Examples/CSharp/MEA2100_Recording_and_Stimulation/Form1.cs
@@ -57,19 +57,77 @@ namespace MEA2100_Recording_and_Stimulation
             }
         }
 
+        private int channelsInBlock = 0;
+        private int threshold = 0;
         private void Dacq_ChannelDataEvent(CMcsUsbDacqNet dacq, int CbHandle, int numFrames)
         {
-            throw new NotImplementedException();
+            List<int[]> data = new List<int[]>();
+            for (int i = 0; i < channelsInBlock / 2; i++)
+            {
+                data.Add(dacq.ChannelBlock_ReadFramesI32(i, threshold, out int frames_ret));
+            }
+
+            BeginInvoke(new HandleDataDelegate(HandleData), data[60], data[68]);
+        }
+
+        delegate void HandleDataDelegate(int[] data1, int[] data2);
+
+        private void HandleData(int[] data1, int[] data2)
+        {
+            FillSeried(0, data1);
+            FillSeried(1, data2);
+        }
+
+        private void FillSeried(int serie, int[] data)
+        {
+            chart1.Series[serie].Points.Clear();
+            for (int i = 0; i < data.Length; i++)
+            {
+                chart1.Series[serie].Points.AddXY(i, data[i]);
+            }
         }
 
         private void btRecordingStart_Click(object sender, EventArgs e)
         {
+            CMcsUsbListEntryNet deviceEntry = (CMcsUsbListEntryNet) cbDeviceList.SelectedItem;
+            uint status = dacq.Connect(deviceEntry);
+            if (status == 0)
+            {
 
+                dacq.StopDacq(0); // if software hat not stopped sampling correctly before
+
+                int samplerate = 50000;
+                dacq.SetSamplerate(samplerate, 0, 0);
+
+                dacq.SetDataMode(DataModeEnumNet.Signed_32bit, 0);
+                dacq.SetNumberOfAnalogChannels(60, 0, 0, 8, 0);
+
+                dacq.EnableDigitalIn(DigitalDatastreamEnableEnumNet.DigitalIn | DigitalDatastreamEnableEnumNet.DigitalOut |
+                                     DigitalDatastreamEnableEnumNet.Hs1SidebandLow | DigitalDatastreamEnableEnumNet.Hs1SidebandHigh, 0);
+                dacq.EnableChecksum(true, 0);
+
+                dacq.GetChannelLayout(out int analogChannels, out int digitalChannels, out int checksumChannels, out int timestampChannels, out channelsInBlock, 0);
+
+                int queuesize = samplerate;
+                threshold = samplerate / 10;
+                dacq.SetSelectedChannels(channelsInBlock / 2, queuesize, threshold, SampleSizeNet.SampleSize32Signed, channelsInBlock);
+
+                dacq.ChannelBlock_SetCommonThreshold(threshold);
+
+                dacq.ChannelBlock_SetCheckChecksum((uint) checksumChannels, (uint) timestampChannels);
+
+                dacq.StartDacq();
+            }
+            else
+            {
+                MessageBox.Show("Connection failed: " + CMcsUsbNet.GetErrorText(status));
+            }
         }
 
         private void btRecordingStop_Click(object sender, EventArgs e)
         {
-
+            dacq.StopDacq(0);
+            dacq.Disconnect();
         }
 
         private void btStimulationStart_Click(object sender, EventArgs e)

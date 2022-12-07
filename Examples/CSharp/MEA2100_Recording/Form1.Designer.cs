ddab32581c80851b9d8c1c415ca64fe21f53783a
Peter Jesinger <jesinger@multichannelsystems.com>
2022-12-07 09:02:33 +0100

    added recording-only example

---
+++ Examples/CSharp/MEA2100_Recording/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace MEA2100_Recording_and_Stimulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btRecordingStart = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btRecordingStop = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.cbDeviceList = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btRecordingStart
+            // 
+            this.btRecordingStart.Location = new System.Drawing.Point(6, 19);
+            this.btRecordingStart.Name = "btRecordingStart";
+            this.btRecordingStart.Size = new System.Drawing.Size(75, 23);
+            this.btRecordingStart.TabIndex = 0;
+            this.btRecordingStart.Text = "Start";
+            this.btRecordingStart.UseVisualStyleBackColor = true;
+            this.btRecordingStart.Click += new System.EventHandler(this.btRecordingStart_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btRecordingStop);
+            this.groupBox1.Controls.Add(this.btRecordingStart);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(172, 51);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Recording";
+            // 
+            // btRecordingStop
+            // 
+            this.btRecordingStop.Location = new System.Drawing.Point(87, 19);
+            this.btRecordingStop.Name = "btRecordingStop";
+            this.btRecordingStop.Size = new System.Drawing.Size(75, 23);
+            this.btRecordingStop.TabIndex = 1;
+            this.btRecordingStop.Text = "Stop";
+            this.btRecordingStop.UseVisualStyleBackColor = true;
+            this.btRecordingStop.Click += new System.EventHandler(this.btRecordingStop_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.AntiAliasing = System.Windows.Forms.DataVisualization.Charting.AntiAliasingStyles.None;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(12, 69);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series2.Name = "Series2";
+            series2.YAxisType = System.Windows.Forms.DataVisualization.Charting.AxisType.Secondary;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(776, 369);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cbDeviceList);
+            this.groupBox3.Location = new System.Drawing.Point(367, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 51);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Device";
+            // 
+            // cbDeviceList
+            // 
+            this.cbDeviceList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDeviceList.FormattingEnabled = true;
+            this.cbDeviceList.Location = new System.Drawing.Point(6, 21);
+            this.cbDeviceList.Name = "cbDeviceList";
+            this.cbDeviceList.Size = new System.Drawing.Size(188, 21);
+            this.cbDeviceList.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(794, 69);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(220, 368);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btRecordingStart;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btRecordingStop;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox cbDeviceList;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Examples/CSharp/MEA2100_Recording/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mcs.Usb;
+
+namespace MEA2100_Recording_and_Stimulation
+{
+    public partial class Form1 : Form
+    {
+        private CMcsUsbListNet list = new CMcsUsbListNet(DeviceEnumNet.MCS_DEVICE_USB);
+
+        private CMeaUSBDeviceNet dacq = new CMeaUSBDeviceNet();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            list.DeviceArrival += List_DeviceArrivalRemoval;
+            list.DeviceRemoval += List_DeviceArrivalRemoval;
+
+            dacq.ErrorEvent += Dacq_ErrorEvent;
+            dacq.ChannelDataEvent += Dacq_ChannelDataEvent;
+
+            RefreshDeviceList();
+        }
+
+        private void RefreshDeviceList()
+        {
+            cbDeviceList.Items.Clear();
+            cbDeviceList.Items.AddRange(list.GetUsbListEntries());
+            if (cbDeviceList.Items.Count > 0)
+            {
+                cbDeviceList.SelectedIndex = 0;
+            }
+        }
+
+        private void List_DeviceArrivalRemoval(CMcsUsbListEntryNet entry)
+        {
+            RefreshDeviceList();
+        }
+
+        private void Dacq_ErrorEvent(string msg, int action)
+        {
+            if (InvokeRequired)
+            {
+                BeginInvoke(new OnError(Dacq_ErrorEvent), msg, action);
+            }
+            else
+            {
+                listBox1.Items.Add(msg);
+            }
+        }
+
+        private int Electrode1 = 0;
+        private int Electrode2 = 1;
+        private int HeadStage = 3;
+
+        private int channelsInBlock = 0;
+        private int threshold = 0;
+        private void Dacq_ChannelDataEvent(CMcsUsbDacqNet dacq, int CbHandle, int numFrames)
+        {
+            List<int[]> data = new List<int[]>();
+            for (int i = 0; i < channelsInBlock / 2; i++)
+            {
+                data.Add(dacq.ChannelBlock_ReadFramesI32(i, threshold, out int frames_ret));
+            }
+
+            if (!inHandleData) // skip if last invoke has not finished yet (weak graphic performance)
+            {
+                // 0 - 59: Elektrode Channels
+                // 60 - 67 IFB Analog IFB channels
+                // 68 Digital In/Out
+                // 69 - 74 Sideband channels
+                // 75 - 76 Checksum channels
+                BeginInvoke(new HandleDataDelegate(HandleData), data[Electrode1], data[69 + HeadStage]);
+            }
+        }
+
+        delegate void HandleDataDelegate(int[] data1, int[] data2);
+
+        // Used for weak graphic performance
+        private bool inHandleData = false;
+
+        private void HandleData(int[] data1, int[] data2)
+        {
+            inHandleData = true;
+
+            bool scaled = false;
+            if (scaled)
+            {
+                double[] scaledData = ScaleData(data1, resolutionHS);
+                FillSeries(0, scaledData);
+            }
+            else
+            {
+                FillSeries(0, data1);
+            }
+            FillSeries(1, data2);
+
+            inHandleData = false;
+        }
+
+        private void FillSeries(int serie, int[] data)
+        {
+            chart1.Series[serie].Points.Clear();
+            int length = data.Length;
+            int joined = length / chart1.Width;
+            for (int i = 0; i < length; i++)
+            {
+#if true // workaround for performance 
+                double x = i;
+                double max = Double.MinValue;
+                double min = Double.MaxValue;
+                for (int j = 0; j < joined && i < length; j++, i++)
+                {
+                    if (data[i] > max)
+                    {
+                        max = data[i];
+                    }
+
+                    if (data[i] < min)
+                    {
+                        min = data[i];
+                    }
+                }
+
+                if (max != Double.MinValue)
+                {
+                    chart1.Series[serie].Points.AddXY(x, max);
+                    chart1.Series[serie].Points.AddXY(x, min);
+                }
+#else
+                chart1.Series[serie].Points.AddXY(i, data[i]);
+#endif
+            }
+        }
+
+        private void FillSeries(int serie, double[] data)
+        {
+            chart1.Series[serie].Points.Clear();
+            int length = data.Length;
+            int joined = length / chart1.Width;
+            for (int i = 0; i < length; i++)
+            {
+#if true // workaround for performance 
+                double x = i;
+                double max = Double.MinValue;
+                double min = Double.MaxValue;
+                for (int j = 0; j < joined && i < length; j++, i++)
+                {
+                    if (data[i] > max)
+                    {
+                        max = data[i];
+                    }
+
+                    if (data[i] < min)
+                    {
+                        min = data[i];
+                    }
+                }
+
+                if (max != Double.MinValue)
+                {
+                    chart1.Series[serie].Points.AddXY(x, max);
+                    chart1.Series[serie].Points.AddXY(x, min);
+                }
+#else
+                chart1.Series[serie].Points.AddXY(i, data[i]);
+#endif
+            }
+        }
+
+        private double resolutionHS = 1;
+        private double resolutionIFBADC = 1;
+
+        double[] ScaleData(int[] data, double resolution)
+        {
+            double[] scaled = new double[data.Length];
+            for (int i = 0; i < data.Length; i++)
+            {
+                scaled[i] = resolution * (double) data[i];
+            }
+
+            return scaled;
+        }
+
+        private void btRecordingStart_Click(object sender, EventArgs e)
+        {
+            CMcsUsbListEntryNet deviceEntry = (CMcsUsbListEntryNet) cbDeviceList.SelectedItem;
+            uint status = dacq.Connect(deviceEntry);
+            if (status == 0)
+            {
+                dacq.StopDacq(0); // if software hat not stopped sampling correctly before
+
+                CSCUFunctionNet scu = new CSCUFunctionNet(dacq);
+                scu.SetDacqLegacyMode(false);
+
+                int samplerate = 50000;
+                dacq.SetSamplerate(samplerate, 0, 0);
+
+                dacq.SetDataMode(DataModeEnumNet.Signed_32bit, 0);
+
+                dacq.GetResolutionPerDigit(0, DacqGroupChannelEnumNet.HeadstageElectrodeGroup, out int resHS, out int resHSUnit);
+                resolutionHS = resHS * Math.Pow(10, -resHSUnit);
+
+                dacq.GetResolutionPerDigit(0, DacqGroupChannelEnumNet.InterfaceADCGroup, out int resIFBADC, out int resIFBADCUnit);
+                resolutionIFBADC = resIFBADC * Math.Pow(10, -resIFBADCUnit);
+
+
+                // for MEA2100-Mini it is assumed that only one HS is connected
+                dacq.SetNumberOfAnalogChannels(60, 0, 0, 8, 0);
+
+                dacq.EnableDigitalIn(DigitalDatastreamEnableEnumNet.DigitalIn | DigitalDatastreamEnableEnumNet.DigitalOut |
+                                     DigitalDatastreamEnableEnumNet.Hs1SidebandLow | DigitalDatastreamEnableEnumNet.Hs1SidebandHigh, 0);
+                dacq.EnableChecksum(true, 0);
+
+                // numbers are in 16bit
+                dacq.GetChannelLayout(out int analogChannels, out int digitalChannels, out int checksumChannels, out int timestampChannels, out channelsInBlock, 0);
+
+                int queuesize = samplerate;
+                threshold = samplerate / 10;
+                // channelsInBlock / 2 gives the number of channels in 32bit
+                dacq.SetSelectedChannels(channelsInBlock / 2, queuesize, threshold, SampleSizeNet.SampleSize32Signed, channelsInBlock);
+
+                dacq.ChannelBlock_SetCommonThreshold(threshold);
+
+                dacq.ChannelBlock_SetCheckChecksum((uint) checksumChannels, (uint) timestampChannels);
+
+                dacq.StartDacq();
+            }
+            else
+            {
+                MessageBox.Show("Connection failed: " + CMcsUsbNet.GetErrorText(status));
+            }
+        }
+
+        private void btRecordingStop_Click(object sender, EventArgs e)
+        {
+            dacq.StopDacq(0);
+            dacq.Disconnect();
+        }
+
+    }
+}
